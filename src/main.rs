c008cba6ab9ad1b2591b7eb2a41630d6068ea1f5
Benoît Vandanjon <benoit@vandanjon.fr>
2024-03-20 16:13:26 +0100

    testing function writing rules

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use std::io;
-
 fn main() {
-    println!("Salut boyz");
-
-    println!("Entrez le premier nombre : ");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Erreur lors de la lecture de l'entrée");
-    let _number: i32 = input
-        .trim()
-        .parse()
-        .expect("Erreur lors de la conversion du nombre");
+    println!("{}, {crowd} {1}!", "Hello", 2 * 21, crowd = "World");
 }
-
-// println!("Entrez le premier nombre : ");

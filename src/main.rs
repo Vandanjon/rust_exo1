64156affe2e3c2568deacaa410a61e84d86d7360
Benoît Vandanjon <benoit@vandanjon.fr>
2024-03-20 16:16:35 +0100

    test macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("{}, {crowd} {1}!", "Hello", 2 * 21, crowd = "World");
+macro_rules! customPrint {
+    () => {
+        println!("Hello, World!")
+    };
+
+    ($dest:expr) => {
+        println!("Hello, {}!", $dest);
+    };
+
+    ($act:expr => $dest:expr) => {
+        println!("{} for {}!", $act, $dest);
+    };
 }
+
+fn main() {
+    customPrint!()
+}
+
+// semi-colon at line 7 and 11 will cause an error

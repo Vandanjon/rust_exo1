a2a09cea28bd4cb9b29ce88121d6b94088898c0d
Benoît Vandanjon <benoit@vandanjon.fr>
2024-03-21 08:38:08 +0100

    add and use a new dep

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-macro_rules! customPrint {
-    () => {
-        println!("Hello, World!")
-    };
-
-    ($dest:expr) => {
-        println!("Hello, {}!", $dest);
-    };
-
-    ($act:expr => $dest:expr) => {
-        println!("{} for {}!", $act, $dest);
-    };
-}
+use num_cpus::get;
 
 fn main() {
-    customPrint!()
+    println!("Number of CPUs: {}", get());
 }
-
-// semi-colon at line 7 and 11 will cause an error
